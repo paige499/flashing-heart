8f94b1d032616ee65bd4b6907b4aa0c9ef3713ba
paige499 <117690905+paige499@users.noreply.github.com>
2022-11-08 11:40:54 +1300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.showIcon(IconNames.SmallHeart)
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+basic.showIcon(IconNames.Heart)
+if (true) {
+    basic.showString("Hello!")
+}
+basic.clearScreen()
+basic.forever(function () {
+	
+})
